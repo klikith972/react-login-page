fa29d975f615daec14aa0427c1dc8f67f1527c73
klikith972 <107628352+klikith972@users.noreply.github.com>
2023-09-04 17:10:18 +0530

    loginpage

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+// src/Login.js
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ username: '', password: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+    // Add your login logic here
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
